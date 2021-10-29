1fcb11d59457d4fb7a5574cd8c854d3e1062cb57
nmitchell313 <nmitchell11@cnm.edu>
2021-10-29 15:02:35 -0600

    parameterized tests for palindromes.

---
+++ src/test/java/edu/cnm/deepdive/PalindromesTest.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class PalindromesTest {
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "palindromes.csv", numLinesToSkip = 1)
+  void checkRecursive(String input, boolean expected) {
+    assertEquals(expected, Palindromes.checkRecursive(input));
+  }
+}
